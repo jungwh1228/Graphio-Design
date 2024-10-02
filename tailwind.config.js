e99f401fbe8911cd6930f3ec06213fd7143d90ec
jungseolhwa <dhqkfskrwl@naver.com>
2024-10-02 13:17:42 +0900

    [IG-172] 추가 설치 및 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './index.html',
+    './css/**/*.scss',
+  ],
+
   theme: {
     extend: {
       fontFamily: {
