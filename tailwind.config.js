ed1704c1a207b9731d86b9dba3bc67a55d26ef7f
jungseolhwa <dhqkfskrwl@naver.com>
2024-10-02 16:19:10 +0900

    [IG-172] 파일 경로 수정, 명령어 수정, 폰트 스타일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './css/**/*.scss',
+    './*.{html,js}',
   ],
-
   theme: {
     extend: {
       fontFamily: {
